376fecd8e879c9d002ceee011f716e869869cac5
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-06 16:08:43 -0300

    Improve original design for the Auth page

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -5,19 +5,22 @@ const Auth = () => {
   return (
     <div className="loginsignup">
       <div className="signup-container">
-        <h1>Sign Up</h1>
+        <h1>Create Account</h1>
         <div className="signup-fields">
           <input type="text" placeholder="Your name" />
-          <input type="email" placeholder="Your email" />
-          <input type="password" placeholder="Your password" />
+          <input type="email" placeholder="Email address" />
+          <input type="password" placeholder="Password" />
         </div>
-        <button>Sign Up</button>
+        <button>Get Started</button>
         <p className="signup-login">
-          Already have an account? <Link to="/login">Login</Link>
+          Already have an account? <Link to="/login">Sign in</Link>
         </p>
         <div className="signup-agree">
-          <input type="checkbox" name="" id="" />
-          <p>By creating an account, I agree to the terms & conditions</p>
+          <input type="checkbox" name="terms" id="terms" />
+          <p>
+            By creating an account, I agree to the Terms of Service and Privacy
+            Policy
+          </p>
         </div>
       </div>
     </div>
