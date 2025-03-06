b9e463af74ad98de392893a5bd38aae15958ca16
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-06 11:56:57 -0300

    Add Hero component and styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; // TODO => switch to createBrowserRouter
+import Shop from "./pages/Shop";
+import ShopCategory from "./pages/ShopCategory";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Auth from "./pages/Auth";
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route path="/men" element={<ShopCategory category="men" />} />
+        <Route path="/women" element={<ShopCategory category="women" />} />
+        <Route path="/kids" element={<ShopCategory category="kids" />} />
+        <Route path="/product" element={<Product />}>
+          <Route path=":productId" element={<Product />} />
+        </Route>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Auth />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
