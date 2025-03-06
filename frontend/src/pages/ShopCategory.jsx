727440624ac62b03b28e0635b43c786c2a56be7e
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-06 15:52:58 -0300

    Add styles to ShopCategory and Load More button. Button not functional yet

--- frontend/src/pages/ShopCategory.jsx
+++ frontend/src/pages/ShopCategory.jsx
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";
 import dropdown_icon from "../components/assets/dropdown_icon.png";
 import Item from "../components/item/Item";
+import "./CSS/ShopCategory.css";
 
 const ShopCategory = (props) => {
   const context = useContext(ShopContext);
@@ -10,17 +11,17 @@ const ShopCategory = (props) => {
   console.log(context);
 
   return (
-    <div className="shop-category">
-      <img src={props.banner} alt="" />
-      <div className="shopcategory-indexSort">
+    <div className="product-category-container">
+      <img className="category-banner" src={props.banner} alt="" />
+      <div className="product-filter-bar">
         <p>
-          <span>Showing 1-12 of 36 products</span>
+          <span>Showing 1-12 </span>of 36 products
         </p>
-        <div className="shopcategory-sort">
+        <div className="sort-dropdown">
           Sort by <img src={dropdown_icon} alt="" />
         </div>
       </div>
-      <div className="shopcategory-products">
+      <div className="product-grid">
         {all_product && all_product.length > 0 ? (
           all_product.map((item, index) => {
             if (item.category === props.category) {
@@ -42,6 +43,7 @@ const ShopCategory = (props) => {
           <p>No products available.</p>
         )}
       </div>
+      <div className="load-more-button">Explore more</div>
     </div>
   );
 };
