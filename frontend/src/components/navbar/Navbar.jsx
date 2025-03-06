dd5f0634ef4285f7a327a717ff4a77d42e257fb4
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-06 11:33:22 -0300

    Create Auth, Cart, Product, Shop, ShopCategory component and add routing for them. Remove text decoration on a

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import "./Navbar.css";
 import logo from "../assets/logo.png";
 import cart_icon from "../assets/cart_icon.png";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [menu, setMenu] = useState("shop");
@@ -14,19 +15,25 @@ const Navbar = () => {
       </div>
       <ul className="shop-nav-menu">
         <li onClick={() => setMenu("shop")}>
-          Shop {menu === "shop" && <hr />}
+          <Link to="/">Shop {menu === "shop" && <hr />}</Link>
         </li>
-        <li onClick={() => setMenu("men")}>Men {menu === "men" && <hr />}</li>
+        <li onClick={() => setMenu("men")}>
+          <Link to="/men">Men {menu === "men" && <hr />}</Link>
+        </li>
         <li onClick={() => setMenu("women")}>
-          Women {menu === "women" && <hr />}
+          <Link to="/women">Women {menu === "women" && <hr />}</Link>
         </li>
         <li onClick={() => setMenu("kids")}>
-          Kids {menu === "kids" && <hr />}
+          <Link to="/kids">Kids {menu === "kids" && <hr />}</Link>
         </li>
       </ul>
       <div className="shop-nav-login-cart">
-        <button>Login</button>
-        <img src={cart_icon} alt="cart" />
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/cart">
+          <img src={cart_icon} alt="cart" />
+        </Link>
         <div className="shop-nav-cart-count">0</div>
       </div>
     </div>
