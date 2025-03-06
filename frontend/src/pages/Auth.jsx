ce233b33b9efe93fca43633b31c6ed7e849dcc70
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-06 16:06:09 -0300

    Create Auth JSX and CSS for signup and login

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,5 +1,27 @@
+import { Link } from "react-router-dom";
+import "./CSS/Auth.css";
+
 const Auth = () => {
-  return <div>Auth</div>;
+  return (
+    <div className="loginsignup">
+      <div className="signup-container">
+        <h1>Sign Up</h1>
+        <div className="signup-fields">
+          <input type="text" placeholder="Your name" />
+          <input type="email" placeholder="Your email" />
+          <input type="password" placeholder="Your password" />
+        </div>
+        <button>Sign Up</button>
+        <p className="signup-login">
+          Already have an account? <Link to="/login">Login</Link>
+        </p>
+        <div className="signup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By creating an account, I agree to the terms & conditions</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Auth;
