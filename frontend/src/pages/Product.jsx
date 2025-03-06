454fb44cc3607b6d4d23805ec2ab3fe9a6cfc725
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-06 17:15:59 -0300

    Create ProductDisplay component and its styles

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,7 +1,9 @@
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { useContext } from "react";
+
 import Breadcrumb from "../components/breadcrumbs/breadcrumb";
+import ProductDisplay from "../components/productDisplay/ProductDisplay";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
@@ -11,6 +13,7 @@ const Product = () => {
   return (
     <div>
       <Breadcrumb product={product} />
+      <ProductDisplay product={product} />
     </div>
   );
 };
