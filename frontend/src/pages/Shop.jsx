3501ec9df7125c7d8b62c561469a9b778e4190ba
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-06 12:33:22 -0300

    Create Offer component and styles

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -1,11 +1,13 @@
 import Hero from "../components/hero/Hero";
 import Popular from "../components/popular/Popular";
+import Offers from "../components/offers/Offers";
 
 const Shop = () => {
   return (
     <div>
       <Hero />
       <Popular />
+      <Offers />
     </div>
   );
 };
