cde7e472f43bf81306a5a8dcd157afb5daf74766
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-06 16:42:36 -0300

    Create breadcrumb component. Add Link to product based on props id and implement it on product jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,5 +1,18 @@
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+import { useContext } from "react";
+import Breadcrumb from "../components/breadcrumbs/breadcrumb";
+
 const Product = () => {
-  return <div>Product</div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+
+  return (
+    <div>
+      <Breadcrumb product={product} />
+    </div>
+  );
 };
 
 export default Product;
