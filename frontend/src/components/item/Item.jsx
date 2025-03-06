8771b2790afa7e1216a8db8369ac9a7ffe3ca827
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-06 14:04:28 -0300

    Add missing dollar sign

--- frontend/src/components/item/Item.jsx
+++ frontend/src/components/item/Item.jsx
@@ -6,8 +6,8 @@ const Item = (props) => {
       <img src={props.image} alt="" />
       <p>{props.name}</p>
       <div className="item-price-container">
-        <div className="item-price-current">{props.new_price}</div>
-        <div className="item-price-previous">{props.old_price}</div>
+        <div className="item-price-current">${props.new_price}</div>
+        <div className="item-price-previous">${props.old_price}</div>
       </div>
     </div>
   );
