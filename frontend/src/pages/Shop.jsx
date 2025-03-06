4955bf2e4ce605291a670091a7e37e50bc86e781
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-06 14:20:57 -0300

    Add newsletter component and styles

--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -2,6 +2,7 @@ import Hero from "../components/hero/Hero";
 import Popular from "../components/popular/Popular";
 import Offers from "../components/offers/Offers";
 import NewCollection from "../components/newCollections/NewCollections";
+import Newsletter from "../components/newsLetter/NewsLetter";
 
 const Shop = () => {
   return (
@@ -10,6 +11,7 @@ const Shop = () => {
       <Popular />
       <Offers />
       <NewCollection />
+      <Newsletter />
     </div>
   );
 };
