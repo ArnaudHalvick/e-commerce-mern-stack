19ad18e6d85fc6dfc7b927ef57557cf15dbc44d4
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-06 12:15:09 -0300

    Create Popular and Item components with styles

---
+++ frontend/src/components/item/Item.jsx
@@ -0,0 +1,16 @@
+import "./Item.css";
+
+const Item = (props) => {
+  return (
+    <div className="item-container">
+      <img src={props.image} alt="" />
+      <p>{props.name}</p>
+      <div className="item-price-container">
+        <div className="item-price-current">{props.new_price}</div>
+        <div className="item-price-previous">{props.old_price}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Item;
---
+++ frontend/src/components/popular/Popular.jsx
@@ -0,0 +1,25 @@
+import "./Popular.css";
+import data_product from "../assets/data";
+import Item from "../item/Item";
+
+const Popular = () => {
+  return (
+    <div className="popular-container">
+      <h1>Popular For Women</h1>
+      <hr />
+      <div className="popular-items">
+        {data_product.map((item) => (
+          <Item
+            key={item.id}
+            image={item.image}
+            name={item.name}
+            new_price={`$${item.new_price}`}
+            old_price={`$${item.old_price}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Popular;
