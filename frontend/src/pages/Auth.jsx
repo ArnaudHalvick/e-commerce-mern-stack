e8c76b656bf00573c674e77e044693b499f36307
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-10 15:30:02 -0300

    Added some basic signup logic. I will have to rework everything later because this is not good enough

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -6,9 +6,42 @@ import { useState } from "react";
 
 const Auth = () => {
   const [state, setState] = useState("Signup");
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
 
   const switchState = () => {
     setState((prevState) => (prevState === "Signup" ? "Login" : "Signup"));
+  };
+
+  const changeHandler = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  //TODO: Replace these 2 functions by one handleSubmit function
+  const login = () => {};
+
+  const signup = async () => {
+    let responseData;
+    await fetch("http://localhost:4000/signup", {
+      method: "POST",
+      body: JSON.stringify(formData),
+      headers: {
+        Accept: "application/form-data",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        responseData = data;
+      });
+
+    if (responseData.success) {
+      localStorage.setItem("auth-token", responseData.token);
+      window.location.replace("/"); // Replace this with React Router Navigate
+    }
   };
 
   return (
@@ -16,11 +49,33 @@ const Auth = () => {
       <div className="signup-container">
         <h1>{state === "Signup" ? "Create Account" : "Login"}</h1>
         <div className="signup-fields">
-          {state === "Signup" && <input type="text" placeholder="Your name" />}
-          <input type="email" placeholder="Email address" />
-          <input type="password" placeholder="Password" />
+          {state === "Signup" && (
+            <input
+              type="text"
+              placeholder="Your name"
+              name="username"
+              value={formData.username}
+              onChange={changeHandler}
+            />
+          )}
+          <input
+            type="email"
+            placeholder="Email address"
+            name="email"
+            value={formData.email}
+            onChange={changeHandler}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={formData.password}
+            onChange={changeHandler}
+          />
         </div>
-        <button>Get Started</button>
+        <button onClick={state === "Signup" ? signup : login}>
+          {state === "Signup" ? "Create Account" : "Login"}
+        </button>
         <p className="signup-login">
           {state === "Signup"
             ? "Already have an account? "
@@ -31,6 +86,7 @@ const Auth = () => {
         </p>
         {state === "Signup" && (
           <div className="signup-agree">
+            {/* TODO: Add some fake links to the terms and privacy policy and prevent the user from signing up if they don't agree */}
             <input type="checkbox" name="terms" id="terms" />
             <p>
               By creating an account, I agree to the Terms of Service and
