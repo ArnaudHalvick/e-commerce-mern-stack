ab2453e996679b0d982ae6f91e9d399b1c136850
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-10 10:06:37 -0300

    Create basic API to add images and products. This is not good practice but I stick to it for now and will improve it later

--- backend/index.js
+++ backend/index.js
@@ -25,6 +25,100 @@ mongoose
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
+// API Creation
+app.get("/", (req, res) => {
+  res.send("Express app is running");
+});
+
+// Image Storage Engine
+const storage = multer.diskStorage({
+  destination: "./upload/images",
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const upload = multer({ storage });
+
+// Creating Upload Endpoint for images
+app.use("/images", express.static(path.join(__dirname, "upload/images")));
+app.post("/upload", upload.single("product"), (req, res) => {
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${port}/images/${req.file.filename}`,
+  });
+});
+
+// Schema for the product
+// TODO: More schema to its own file
+const Product = new mongoose.model("Product", {
+  id: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  new_price: {
+    type: Number,
+    required: true,
+  },
+  old_price: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  available: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+app.post("/add-product", async (req, res) => {
+  let products = await Product.find();
+  let id;
+
+  // TODO: Modify the way ids are generated because this is not a good practice
+  if (products.length > 0) {
+    id = products[products.length - 1].id + 1;
+  } else {
+    id = 1;
+  }
+
+  const product = new Product({
+    id: id,
+    image: req.body.image,
+    name: req.body.name,
+    category: req.body.category,
+    new_price: req.body.new_price,
+    old_price: req.body.old_price,
+    date: req.body.date,
+    available: req.body.available,
+  });
+  console.log(product);
+  await product.save();
+  res.json({
+    success: true,
+    name: req.body.name,
+  });
+});
+
 app.listen(port, (error) => {
   if (error) {
     console.log(error);
