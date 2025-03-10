63a7d90b8c493a6edd473244f3f86cf9ba80c1ee
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-10 15:09:10 -0300

    Add state to switch between account creation and signup on Auth.jsx

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,29 +1,43 @@
-import { Link } from "react-router-dom";
 import "./CSS/Auth.css";
 
+import { Link } from "react-router-dom";
+import { useState } from "react";
 // TODO: Work on responsiveness and auth pages after backend is done
 
 const Auth = () => {
+  const [state, setState] = useState("Signup");
+
+  const switchState = () => {
+    setState((prevState) => (prevState === "Signup" ? "Login" : "Signup"));
+  };
+
   return (
     <div className="loginsignup">
       <div className="signup-container">
-        <h1>Create Account</h1>
+        <h1>{state === "Signup" ? "Create Account" : "Login"}</h1>
         <div className="signup-fields">
-          <input type="text" placeholder="Your name" />
+          {state === "Signup" && <input type="text" placeholder="Your name" />}
           <input type="email" placeholder="Email address" />
           <input type="password" placeholder="Password" />
         </div>
         <button>Get Started</button>
         <p className="signup-login">
-          Already have an account? <Link to="/login">Sign in</Link>
+          {state === "Signup"
+            ? "Already have an account? "
+            : "Don't have an account? "}
+          <Link to="/login" onClick={switchState}>
+            {state === "Signup" ? "Sign in" : "Sign up"}
+          </Link>
         </p>
-        <div className="signup-agree">
-          <input type="checkbox" name="terms" id="terms" />
-          <p>
-            By creating an account, I agree to the Terms of Service and Privacy
-            Policy
-          </p>
-        </div>
+        {state === "Signup" && (
+          <div className="signup-agree">
+            <input type="checkbox" name="terms" id="terms" />
+            <p>
+              By creating an account, I agree to the Terms of Service and
+              Privacy Policy Policy
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
