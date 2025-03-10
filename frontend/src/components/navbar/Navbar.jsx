0a0445d1d58b111ead333dfcfaf41bc5ae787e56
ArnaudHalvick <arnaud.halvick@gmail.com>
2025-03-10 15:37:17 -0300

    Added logout button

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -52,9 +52,20 @@ const Navbar = () => {
       </ul>
 
       <div className="shop-nav-login-cart">
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
+        {localStorage.getItem("auth-token") ? (
+          <button
+            onClick={() => {
+              localStorage.removeItem("auth-token");
+              window.location.reload();
+            }}
+          >
+            Logout
+          </button>
+        ) : (
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        )}
         <Link to="/cart">
           <img src={cart_icon} alt="cart" />
         </Link>
